15835042d3877b683e8de4992ae81310badc0d45
Anghelypcc <anghelycenteno334@gmail.com>
2025-05-14 11:07:50 +0000

    first commit

---
+++ i.py
@@ -0,0 +1,29 @@
+numero=-3.1416
+pi=-numero
+print(pi) 
+variable=1
+variable1= "indigo"
+variable2= True
+variable3= None
+comparacion=variable=1
+print(comparacion)
+
+
+print(variable)
+
+variable=1
+anghely="anghelycenteno334@gmail.com" 
+comparacion=variable==1
+print(comparacion)
+comparacion=not comparacion 
+print(comparacion)
+mayor=variable>0
+print(mayor)
+menor=variable<=0
+print(menor)
+
+mayor=(1+1)>1
+menor=(20-30)<10
+y=mayor and menor
+print(y, "and")
+ 
