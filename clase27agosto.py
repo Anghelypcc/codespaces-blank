abe30cdd0751525b57ce3b7e179a8480a097448a
Anghelypcc <anghelycenteno334@gmail.com>
2025-08-27 11:59:49 +0000

    ueh

---
+++ clase27agosto.py
@@ -0,0 +1,59 @@
+import math
+
+raiz_cuadrada =math.sqrt (144)
+
+print(raiz_cuadrada)
+
+raz_cubica = math. cbrt (entero)
+entero= int (raiz cubica)
+
+#podemos hacer raices elevando un numero a un exponente de fraccior
+
+
+igual
+raiz
+_cuadrada == 144 **(1/2)
+#usar math.pow es lo mismo que usar **
+
+numero_al_cuadrado = math. pow (144,2)
+
+igualar_cuadrados = numero_al_cuadrado == 144 ** 2
+
+print("es lo mismo usar pow que ***, igualar_cuadrados)
+
+print (igual)
+
+raiz_cuadratica
+16 ** (1/4)
+igualar_cuadraticas = raiz_cuadratica == math.pow(16,1/4)
+
+print (raiz_cuadratica)
+
+
+print ("las raices cuadraticas son iguales?", igualar_cuadraticas)
+
+#print(igual)
+
+
+numero_pi-math.pi
+
+print (numero_pi)
+pi_al_cuadrado = numero_pi ** 2
+print(pi_al_cuadrado)
+numero_de_eurler = math.e
+print(numero_de_eurler)
+logaritmo_base_e = math.log(144)
+print(logaritmo_base_e)
+logaritmo_base_e_2 = math.log2(144)
+print(logaritmo_base_e_2)
+infinito= math.inf
+print(infinito/2)
+
+def raiz(tadicando, indice):
+    return radicando ** (1/indice)
+
+raiz_sexta = raiz(512,2)
+pint(raiz_sexta)
+
+
+
